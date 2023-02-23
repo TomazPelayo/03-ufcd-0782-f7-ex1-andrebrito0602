c1a7a3ff443a27f28cc8cf045533797e3ab83548
andrebrito0602 <122288809+andrebrito0602@users.noreply.github.com>
2023-02-23 17:19:58 +0000

    aaa

--- exercicio1.c
+++ exercicio1.c
@@ -1,2 +1,27 @@
 // Escreva o seu código a seguir
+#include<stdio.h>
+int main () {
+int a;
+int a1;
+int a2;
+int res;
+char palavra[80];
+printf("O seu nome\n");
+gets(palavra);
+printf("Dia de Nascimento:\n");
+scanf("%d",&a);
+printf("Mes de Nascimento:\n");
+scanf("%d",&a1);
+printf("Ano de Nascimento:\n");
+scanf("%d",&a2);
 
+res= 365*2023 + 30*2 + 15 -365*a2 -30*a1 - a;
+
+
+printf("+-------------------+");
+printf("|                   |");
+printf("|                   |");
+printf("+-------------------+");
+printf("|                   |");
+printf("+-------------------+");
+}
